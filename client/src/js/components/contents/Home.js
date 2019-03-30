deacb8c69bcb5756b1eecb378e4733b86561450e
Morris Lin <snoopy90338@gmail.com>
2019-03-30 16:40:34 +0800

    UI

--- client/src/js/components/contents/Home.js
+++ client/src/js/components/contents/Home.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react'
 export default class Home extends Component {
 	render() {
 		return (
-			<h1>Home</h1>
+			<div className='mr-0-100 up-mr-30'>
+				<h1>Umbrella</h1>
+				<h2>App that helps you find your umbrella.</h2>
+			</div>
 		)
 	}
 }
