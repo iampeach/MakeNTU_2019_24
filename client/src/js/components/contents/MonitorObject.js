5bb58a0c29e299e1555057f34dc350a7bccaf9f0
Morris Lin <snoopy90338@gmail.com>
2019-03-31 05:31:52 +0800

    UI change and add redirect to map

--- client/src/js/components/contents/MonitorObject.js
+++ client/src/js/components/contents/MonitorObject.js
@@ -4,15 +4,17 @@ export default class MonitorObject extends Component {
 	state = {
 		isObject: this.props.isObject || false
 	}
+	map = _ => {
+		if (this.state.isObject)
+			window.open('https://www.google.com/maps/place/'+this.props.object.lat+this.props.object.long)
+	}
 	render() {
-		var nameStyle = 'w-200 pd-20 mr-10 bold f4'
-		if (this.state.isObject) nameStyle = nameStyle + ' bg-washed-blue'
+		var nameStyle = 'w-300 pd-20 mr-10 bold'
+		if (this.state.isObject) nameStyle = nameStyle + ' bg-washed-blue cursor-point'
 		return(
 			<div className='mr-0-100 fl-row align-center'>
-				<h2 className={nameStyle}>{this.props.object.name}</h2>
-				<h3 className='w-100 pd-20 light f5'>{this.props.object.time}</h3>
-				<h3 className='w-100 pd-20 light f5'>{this.props.object.lat}</h3>
-				<h3 className='w-100 pd-20 light f5'>{this.props.object.long}</h3>
+				<h2 className={nameStyle} onClick={()=>this.map()}>{this.props.object.name}</h2>
+				<h3 className='w-300 pd-20 light'>{this.props.object.time}</h3>
 			</div>
 		)
 	}
