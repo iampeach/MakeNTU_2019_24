c6a07645a6d76127c5b8a3c6fb33f87f47e5e3c5
Morris Lin <snoopy90338@gmail.com>
2019-03-30 14:43:38 +0800

    add route header

--- client/src/js/App.js
+++ client/src/js/App.js
@@ -1,16 +1,27 @@
 import React, {Component} from 'react'
 import Header from './components/Header'
 import Content from './components/Content'
+import { withRouter } from 'react-router'
 
 class App extends Component {
+	state = {
+		contents: [
+			{ url: '/', text: 'Home' },
+			{ url: '/monitor', text: 'Monitor' },
+			{ url: '/register', text: 'Register' },
+		]
+	}
+	changePage = id => {
+		this.props.history.push(this.state.contents[id].url)
+	}
 	render() {
 		return (
 			<div className='mg-0 pd-0 fl-col'>
-				<Header />
+				<Header changePage={this.changePage} contents={this.state.contents}/>
 				<Content />
 			</div>
 		)
 	}
 }
 
-export default App
+export default withRouter(App)
--- client/src/js/components/Header.js
+++ client/src/js/components/Header.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react'
 
 export default class Header extends Component {
+	changePage = id => {
+		this.props.changePage(id)
+	}
 	render() {
+		var caption_style = 'grey light up-mr-10 d-mr-10 pd-20 fl-0-1 f4 cursor-point'
+		var captions = this.props.contents.map((content, idx) => 
+			<h2 className={caption_style} 
+					onClick={()=>this.changePage(idx)}
+					key={idx}>
+						{content.text}
+			</h2>
+		)
 		return (
-			<h1>Header</h1>
+			<div className='mr-0 d-mr-50 pd-0 fl-row fl-end font-pri'>
+				{captions}
+			</div>
 		)
 	}
 }
