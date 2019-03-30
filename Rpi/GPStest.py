3706d9efb2fa3cc346159a9ce19155bdb4077ea1
whoami90506 <whoami90506@github.com>
2019-03-30 15:13:04 +0800

    test gps finish

--- Rpi/GPStest.py
+++ Rpi/GPStest.py
@@ -1,1 +1,14 @@
 import serial
+
+#init
+gps = serial.Serial('/dev/ttyS0', baudrate = 9600, timeout = 0.5)
+#gps.xonxoff = 1
+
+#run
+#gps.open()
+if gps.isOpen() :
+	print('success to open.')
+	while True :
+		print(gps.readline())
+else :
+	print('fail to open gps.')
