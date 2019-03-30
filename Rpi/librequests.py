1ec0956aaf668e101b1ba52041e5200912f97573
whoami90506 <whoami90506@github.com>
2019-03-31 05:07:27 +0800

    fix location bug

--- Rpi/librequests.py
+++ Rpi/librequests.py
@@ -4,8 +4,8 @@ import random
 
 def sendBackEnd(url, lat, lon):
 	d = dict()
-	if lat is None : lat = 25.045723 + random.random()/1000 - 0.5
-	if lon is None : lon = 121.531018 + random.random()/1000 - 0.5
+	if lat is None : lat = 25.045723 + (random.random()-0.5)/10000
+	if lon is None : lon = 121.531018 + (random.random()-0.5)/10000 
 	lat = str(lat)[:8] + 'N' if lat >=0 else str(-1*lat)[:8] + 'S'
 	lon = str(lon)[:9] + 'E' if lon >=0 else str(-1*lon)[:9] + 'W'
 
