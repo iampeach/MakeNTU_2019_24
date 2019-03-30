0bd4fb80cd4e59b432afeadf232009e003842162
Morris Lin <snoopy90338@gmail.com>
2019-03-31 01:36:59 +0800

    rename project

--- client/src/js/components/contents/Home.js
+++ client/src/js/components/contents/Home.js
@@ -4,8 +4,8 @@ export default class Home extends Component {
 	render() {
 		return (
 			<div className='mr-0-100 up-mr-30'>
-				<h1>Umbrella</h1>
-				<h2>App that helps you find your umbrella.</h2>
+				<h1>Water Bottle</h1>
+				<h2>App that helps you find your water bottle.</h2>
 			</div>
 		)
 	}
