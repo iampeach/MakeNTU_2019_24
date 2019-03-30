b1b4cc5cd7696e7d355a8b164f1dbec73ce7f6a3
whoami90506 <whoami90506@github.com>
2019-03-30 15:58:32 +0800

    find garbage gps

--- Rpi/GPStest.py
+++ Rpi/GPStest.py
@@ -2,13 +2,13 @@ import serial
 
 #init
 gps = serial.Serial('/dev/ttyS0', baudrate = 9600, timeout = 0.5)
-#gps.xonxoff = 1
 
 #run
-#gps.open()
-if gps.isOpen() :
-	print('success to open.')
-	while True :
-		print(gps.readline())
-else :
-	print('fail to open gps.')
+while True:
+	data = gps.readline()
+	if not data or data == '\n' or data[0:6] == '$GPTXT': continue
+	print(data)
+	data = data.split(',')
+	for d in data : 
+		if d : print(d)
+	print('-----')
